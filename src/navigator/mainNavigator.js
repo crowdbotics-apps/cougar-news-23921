da98c07baa69c11fe26437b66546a9b56bcb92f6
Crowdbotics <team@crowdbotics.com>
2021-01-19 02:44:18 +0000

    add package blueprint-react-native-articles-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import ArticleList194378Navigator from '../features/ArticleList194378/navigator';
 import SignUp21194377Navigator from '../features/SignUp21194377/navigator';
 import Settings3194375Navigator from '../features/Settings3194375/navigator';
 import CalendarView4194374Navigator from '../features/CalendarView4194374/navigator';
@@ -16,6 +17,7 @@ import CalendarView4194374Navigator from '../features/CalendarView4194374/naviga
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+ArticleList194378: { screen: ArticleList194378Navigator },
 SignUp21194377: { screen: SignUp21194377Navigator },
 Settings3194375: { screen: Settings3194375Navigator },
 CalendarView4194374: { screen: CalendarView4194374Navigator },
