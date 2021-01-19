e4df566b5cb4c9420f90438ba663524f2dc37e18
Crowdbotics <team@crowdbotics.com>
2021-01-19 02:43:18 +0000

    Update models

--- backend/cougar_news_23921/wsgi.py
+++ backend/cougar_news_23921/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cougar_news_23921.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cougar_news_23921.settings")
 
 application = get_wsgi_application()
